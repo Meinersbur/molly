0eff3274eda99c94a3713f4f37815702e9cff173
Michael Kruse <michael.kruse@inria.fr>
2013-02-09 00:48:07 +0100

    Correct linking of playground

--- tools/playground/playground.cpp
+++ tools/playground/playground.cpp
@@ -1,6 +1,14 @@
 
 #include <cstdlib>
 
+#include "islpp/Ctx.h"
+
+
+void test() {
+  auto ctx = isl::Ctx::create();
+}
+
 int main(int argc_, const char **argv_) {
+ test();
   return EXIT_SUCCESS;
 }
