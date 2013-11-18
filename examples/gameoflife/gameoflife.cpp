09d3633c8bf14614b29b65f6c6c5f3cc426ba0fa
Michael Kruse <github@meinersbur.de>
2013-11-18 11:32:32 +0100

    Run and compile on BG/Q

--- examples/gameoflife/gameoflife.cpp
+++ examples/gameoflife/gameoflife.cpp
@@ -1,10 +1,17 @@
 #include <molly.h>
+#include <iostream>
 
-molly::array<bool,6,6> habitat1;
-molly::array<bool,6,6> habitat2;
+#include <mpi.h>
+
+#define LENGTH (64*8)
+#define ITERATIONS 100
+#define TESTS 3
+
+molly::array<bool,LENGTH,LENGTH> habitat1;
+molly::array<bool,LENGTH,LENGTH> habitat2;
 
 
-[[molly::pure]] static bool hasLife(bool prevHasLife, int neighbors) { MOLLY_DEBUG_FUNCTION_ARGS(prevHasLife, neighbors)
+[[molly::pure]] static bool hasLife(bool prevHasLife, int neighbors) { MOLLY_DEBUG_FUNCTION_SCOPE
   if (prevHasLife)
     return 2 <= neighbors && neighbors <= 3;
   return neighbors == 3;
@@ -28,7 +35,7 @@ extern "C" void test() { MOLLY_DEBUG_FUNCTION_SCOPE
 #endif
 
 extern "C" void test() { MOLLY_DEBUG_FUNCTION_SCOPE 
-  for (auto i = 0; i < 3; i+=1) {
+  for (auto i = 0; i < ITERATIONS; i+=1) {
     for (int x = 0, width = habitat1.length(0); x < width-2; x+=1) {
       for (int y = 0, height = habitat1.length(1); y < height-2; y+=1) {
 
@@ -61,7 +68,21 @@ extern "C" void test() { MOLLY_DEBUG_FUNCTION_SCOPE
 
 int main(int argc, char *argv[], char *envp[]) { MOLLY_DEBUG_FUNCTION_SCOPE
 
-  test();
-
+  test(); // Warmup
+  
+  auto starttime = MPI_Wtime();
+  for (auto i = 0; i < TESTS; i+=1) {
+    test();
+  }
+  auto stoptime = MPI_Wtime();
+  auto duration = stoptime - starttime;
+  
+  if (__molly_cluster_mympirank()==PRINTRANK) {
+      auto lup = 1.0*(LENGTH-2)*(LENGTH-2)*ITERATIONS*TESTS;
+      std::cout << "Fieldsize " << LENGTH << "x" << LENGTH << ", " << ITERATIONS << " iterations, " << TESTS << " tests (" << lup << " lattice updates)\n";
+      std::cout << "Time " <<  duration << "s total, " << (duration/TESTS) << "s per test, " << (duration/(TESTS*ITERATIONS)) << "s per iterations, " << (duration/lup) << "s per lup\n";
+      std::cout <<  ((lup*1e6) / duration) << " mlups" << std::endl;
+  }
+  
   return EXIT_SUCCESS;
 }
